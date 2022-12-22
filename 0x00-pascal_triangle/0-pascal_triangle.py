0b208cd8867c9027f98a5eb14b3dbd6e0b503ced
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2022-12-22 20:58:45 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,10 @@
-def pascal_triangle(n):
-	for i in range(n):
-	print(' '*(n-i), end='')
-	print(' '.join(map(str, str(11**i))))
+def pascal_triangle_generator():
+    la=[1]
+    while True:
+        for i in la:
+            yield i
+        yield None
+        lb=[1]
+        lb.extend(i+j for i, j in zip(la[:-1], la[1:]))
+        lb.append(1)
+        la=lb
