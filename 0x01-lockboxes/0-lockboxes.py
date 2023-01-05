182eba3534b23bd998777dfa01f12df9b4fc1ff5
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2023-01-05 12:13:02 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
+def canUnlockAll(boxes):
+    """
+    This function takes a list of boxes as input, where each box is a list of keys.
+    The function returns True if all the boxes can be opened, else it returns False.
+    """
+    # Initialize a set of keys that are available
+    keys = set()
 
-# Create a queue to store the keys that need to be checked
-queue = [0]
+    # Add the keys in the first box to the set of keys
+    keys.update(boxes[0])
 
-# While there are keys in the queue
-while queue:
-    # Get the key from the queue
-    key = queue.pop(0)
-    # For each box in the list of boxes
-    for box in boxes:
-        # If the key is present in the box
-        if key in box:
-            # Add the box to the set of openable boxes
-            openable_boxes.add(boxes.index(box))
-            # Add the keys in the box to the queue
-            queue += box
+    # Initialize a set of boxes that have been opened
+    opened_boxes = {0}
 
-# If the number of openable boxes is equal to the total number of boxes
-if len(openable_boxes) == len(boxes):
-    # All boxes are openable
-    return True
-# If the number of openable boxes is less than the total number of boxes
-else:
-    # Some boxes are not openable
-    return False
+    #  While there are keys available and not all boxes have been opened,
+    while keys and len(opened_boxes) < len(boxes):
+    
+        # Take a key from the set
+        key = keys.pop()
+    
+        # If the key is a valid box number and the box has not been opened yet,
+        if key < len(boxes) and key not in opened_boxes:
+        
+            # Add the key to the set of opened boxes
+            opened_boxes.add(key)
+        
+            # Add the keys in the box to the set of keys
+            keys.update(boxes[key])
+
+    # Return True if all boxes have been opened, else return False
+    return len(opened_boxes) == len(boxes)
