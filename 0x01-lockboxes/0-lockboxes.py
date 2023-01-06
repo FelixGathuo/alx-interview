46d7fb8813b80873a3753389684f822445b03db0
FelixGathuo <gathuofelix@gmail.com>
2023-01-06 08:24:32 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 '''
-    This function takes a list of boxes as input, where each box is a list of keys.
-    The function returns True if all the boxes can be opened, else it returns False.
+    This function takes a list of boxes as input, where each box
+    is a list of keys.
+    The function returns True if
+    all the boxes can be opened, else it returns False.
 '''
+
+
 def canUnlockAll(boxes):
     '''
-    This function takes a list of boxes as input, where each box is a list of keys.
-    The function returns True if all the boxes can be opened, else it returns False.
+    This function takes a list of boxes as input, where
+    each box is a list of keys.
+    The function returns True if all
+    the boxes can be opened, else it returns False.
     '''
     # Initialize a set of keys that are available
     keys = set()
@@ -17,18 +23,18 @@ def canUnlockAll(boxes):
     # Initialize a set of boxes that have been opened
     opened_boxes = {0}
 
-    #  While there are keys available and not all boxes have been opened,
+#  While there are keys available and not all boxes have been opened,
     while keys and len(opened_boxes) < len(boxes):
-    
+
         # Take a key from the set
         key = keys.pop()
-    
+
         # If the key is a valid box number and the box has not been opened yet,
         if key < len(boxes) and key not in opened_boxes:
-        
+
             # Add the key to the set of opened boxes
             opened_boxes.add(key)
-        
+
             # Add the keys in the box to the set of keys
             keys.update(boxes[key])
 
